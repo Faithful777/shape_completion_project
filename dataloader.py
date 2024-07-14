0744e750be74baf912b4c6699935393e5c431913
Faithful777 <128046605+Faithful777@users.noreply.github.com>
2024-07-14 13:37:17 +0100

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -128,7 +128,7 @@ class ShapeCompletionDataset():
             item['rgbd_intrinsic'] = input_data['intrinsic']
             item['rgbd_frames'] = input_data['frames']
 
-        print(item)
+        print(len(item))
 
         return item
     
